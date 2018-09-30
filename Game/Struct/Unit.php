5b579ddf86e4ce54157d33a478ed09d673b502dd
Chopperl <43599300+Chopperl@users.noreply.github.com>
2018-09-30 18:00:22 +0400

    Update Unit.php

--- Game/Struct/Unit.php
+++ Game/Struct/Unit.php
@@ -6,11 +6,13 @@ class Unit extends BaseElement {
     public $gamerId;
     public $x;
     public $y;
+    public $speed;
 
     public function __construct($options) {
         parent::__construct($options);
         $this->gamerId = $options->gamerId;
         $this->x       = $options->x;
         $this->y       = $options->y;
+        $this->$speed  = $options->speed;
     }
 }
